d9e5da6656605b3ffc258c99962043985aed228a
Emanuel Costi Stefan <emanuel.stefan@tim-mb-175.3pillar.corp>
2019-02-28 11:07:40 +0200

    seed products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "book", price: 3.99)
+Product.create(name: "pen", price: 1.99)
+Product.create(name: "notebook", price: 2)
+Product.create(name: "desktop", price: 100)
+Product.create(name: "charger", price: 10)
