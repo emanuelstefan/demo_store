5385d5ee0307f204e40343c22a7594f9543d9bbd
Emanuel Costi Stefan <emanuel.stefan@tim-mb-175.3pillar.corp>
2019-02-28 14:41:08 +0200

    fixes, adjustments to CRUD actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,13 @@
 class ProductsController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
     @products = Product.all.order("created_at DESC")
 
-    render json: { products: @products }
+    render json: {
+      total_items: @products.size,
+      products: @products
+    }
   end
 
   def list_products
@@ -13,33 +18,64 @@ class ProductsController < ApplicationController
     end
   end
 
-  def show
-    @product = Product.find(params[:id])
-
-    render json: @product
+  def create
+    if params_missing?
+      return render json: { message: "invalid or missing params" }, status: 422
+    else
+      @product = Product.create(product_params)
+      render json: {
+        message: "product created",
+        product: @product
+      }
+    end
   end
 
-  def edit
-    @product = Product.find(params[:id])
-
-    render json: @product
+  def show
+    @product = Product.find_by(id: params[:id])
+    if @product.blank?
+      return render json: { message: "product not found" }, status: 404
+    else
+      render json: @product
+    end
   end
 
   def update
-    @product = Product.find(params[:id])
-    @product.update(product_params)
+    @product = Product.find_by(id: params[:id])
 
-    render json: @product
+    return render json: {message: "invalid or missing params"} if params_missing?
+
+    if @product.blank?
+      return render json: { message: "product not found" }, status: 404
+    else
+      @product.update(product_params)
+
+      render json: {
+        message: "product updated",
+        product: @product
+      }
+    end
   end
 
-  def delete
-    @product = Product.find(params[:id])
-    @product.delete
+  def destroy
+    @product = Product.find_by(id: params[:id])
 
-    render json: @product
+    if @product.blank?
+      return render json: { message: "product not found" }, status: 404
+    else
+      @product.delete
+
+      render json: {
+        message: "product deleted",
+        product: @product
+      }
+    end
   end
 
   private
+  def params_missing?
+    product_params.nil? || product_params[:name].nil? || product_params[:price].nil?
+  end
+
   def product_params
     params.permit(:name, :price)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root 'products#index'
+  root 'products#list_products'
 
   resources :products do
     collection do
       get 'list_products', to: "products#list_products"
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
